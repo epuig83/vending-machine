4d94bd0b81fbef682484195bb031e4dcbea4c727
Edgard Puig <edgardpuig@gmail.com>
2021-09-28 20:09:26 +0200

    NF WIP - create new buyitem route and its controller action

--- src/src/Controller/VendingApiController.php
+++ src/src/Controller/VendingApiController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Exception\CoinException;
+use App\Exception\ItemException;
 use App\Service\VendingService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,21 +11,67 @@ use Symfony\Component\HttpFoundation\Response;
 
 class VendingApiController extends AbstractController
 {
+    /**
+     * @param Request $request
+     * @param VendingService $vendingService
+     * @return Response
+     * @throws \App\Exception\CoinException
+     */
     public function insertCoin(Request $request, VendingService $vendingService): Response
     {
-        $vendingService->insertCoin($request->get('coin'));
-        return $this->json([], Response::HTTP_CREATED);
+        try {
+            $vendingService->insertCoin($request->get('coin'));
+            return $this->json([
+                'success' => true,
+                'message' => 'Coin has been inserted to vending machine successfully'
+            ], Response::HTTP_CREATED);
+        } catch(CoinException $e) {
+            return $this->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], Response::HTTP_OK);
+        }
     }
 
+    /**
+     * @param VendingService $vendingService
+     * @return Response
+     */
     public function returnCoin(VendingService $vendingService): Response
     {
         $coins = $vendingService->returnCoin();
         return $this->json($coins, Response::HTTP_OK);
     }
 
+    /**
+     * @param VendingService $vendingService
+     * @return Response
+     */
     public function getCoinStatus(VendingService $vendingService): Response
     {
         $status = $vendingService->coinStatus();
         return $this->json($status, Response::HTTP_OK);
+    }
+
+    /**
+     * @param Request $request
+     * @param VendingService $vendingService
+     * @return Response
+     */
+    public function getItemBuy(Request $request, VendingService $vendingService): Response
+    {
+        try{
+            $change = $vendingService->buyItem($request->get('name'));
+            return $this->json([
+                'success' => true,
+                'change' => $change
+            ], Response::HTTP_OK);
+        } catch (ItemException $e) {
+            return $this->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], Response::HTTP_OK);
+        }
+
     }
 }
